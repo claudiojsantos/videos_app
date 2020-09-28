528335fab102a3eeb195f03aefa19285766b0735
Cláudio Santos <cljos@hotmail.com>
2020-09-28 13:54:13 -0300

    chore: add page index and new router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'main/index'
   # get 'videos/index'
   # get 'videos/new'
   # get 'videos/create'
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   # get 'videos/update'
   # get 'videos/delete'
 
-  root :to => 'videos#index'
+  root :to => 'main#index'
 
   resources :videos
   
