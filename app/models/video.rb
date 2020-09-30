3bf878fb877124a771c8a64a94e0472b9b74e527
Cláudio Santos <cljos@hotmail.com>
2020-09-30 13:15:52 -0300

    refactor: form and model to upload videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,8 @@ class Video < ApplicationRecord
     validates :name, :url, presence: :true
     validates_numericality_of :views, greater_than_or_equal_to: 0
 
+    mount_uploader :url, VideoUploader
+
     belongs_to :user
 
     def quantity_views
